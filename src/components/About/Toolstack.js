7c4da4e885b337be32cb32ef1e7ca0304cf43d38
Abhishek Gupta <abhishekkrgupta117@gmail.com>
2023-04-08 23:02:39 +0530

    Update Toolstack.js

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  SiLinux,
   SiVisualstudiocode,
-  SiPostman,
-  SiHeroku,
-  SiVercel,
+  SiPostman
 } from "react-icons/si";
 import { DiPostgresql } from "react-icons/di";
 
